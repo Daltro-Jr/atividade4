cbf065efdadea53b1a4b86349c34b39700fc63d8
Daltro-Jr <93282997+Daltro-Jr@users.noreply.github.com>
2021-10-31 18:24:27 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 resource "aws_instance" "servidor_ubuntu" {
   ami = "ami-00399ec92321828f5"
   instance_type = "t2.micro"
-  key_name = "terraform-key"
+  key_name = "key-daltro"
   tags = {
     Name = "servidor_ubuntu_ansible"
   } 
